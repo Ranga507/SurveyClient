710b0dbae90d01202a1585c2cbb95bc9a73ce1c6
Ranga507 <rangababu1009@gmail.com>
2019-08-21 15:16:39 +0530

    Initial Setup Done and added questions lising page

---
+++ src/components/questions/Questions.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Hello World!</h1>
+</template>
+
+<script>
+export default {
+  name: 'Questions',
+  components: {
+  },
+  data () {
+    return {
+      questions: []
+    }
+  },
+  created () {
+  },
+  methods: {
+
+  }
+}
+</script>
